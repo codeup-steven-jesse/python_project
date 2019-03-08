0bb320c572d978e311f5cb4695ba8f3afe29d431
Steven Garis <steven.t.garis@gmail.com>
2019-03-08 13:57:19 -0600

    valadation working

--- checkbook.py
+++ checkbook.py
@@ -5,20 +5,34 @@ with open('current_balance.txt') as f:
     balance = f.read()
 
 
-def validate_user_input(user_decision):
-    while not user_decision.isdigit() or int(user_decision) > 5 or input(user_decision) < 1:
-        return user_decision
-    else:
-        user_decision = input('What would you like to do? \n')
+
+def get_user_input():
+    user_decision = input('''
+    What would you like to do? \n
+    1) View current balance
+    2) Add a debit (withdrawal)
+    3) Add a credit (deposit)
+    4) Exit
+        ''')
+
+    while not user_decision.isdigit() or int(user_decision) > 5 or int(user_decision) < 1:
+        print('Error! Please pick a valid option number.')
+        user_decision = input('''
+What would you like to do? \n
+1) View current balance
+2) Add a debit (withdrawal)
+3) Add a credit (deposit)
+4) Exit\n
+        ''')
+    return user_decision
 
 
-def program(user_decision):
-    if user_decision == 1:
-        print('Your current balance is $' + balance + '.')
-    # elif user_decision == 2:
-    #
-    # elif user_decision == 3:
 
+
+def program():
+    user_decision = get_user_input()
+    if int(user_decision) == 1:
+        print(balance)
     else:
         print('End program')
 
@@ -30,13 +44,5 @@ def program(user_decision):
 
 print('\n~~~ Welcome to your terminal checkbook! ~~~')
 
-user_decision = int(input('''
-    What would you like to do? \n
-    1) View current balance\n
-    2) Add a debit (withdrawal)\n
-    3) Add a credit (deposit)\n
-    4) Exit \n 
-    '''))
 
-
-program(user_decision)
+program()
