51c29ebdec1223862c231a17c6b97c16347805af
Steven Garis <steven.t.garis@gmail.com>
2019-03-08 14:16:27 -0600

    added for paths from check bal

--- checkbook.py
+++ checkbook.py
@@ -8,15 +8,15 @@ with open('current_balance.txt') as f:
 
 def get_user_input():
     user_decision = input('''
-    What would you like to do? \n
-    1) View current balance
-    2) Add a debit (withdrawal)
-    3) Add a credit (deposit)
-    4) Exit
+What would you like to do? \n
+1) View current balance
+2) Add a debit (withdrawal)
+3) Add a credit (deposit)
+4) Exit
         ''')
 
-    while not user_decision.isdigit() or int(user_decision) > 5 or int(user_decision) < 1:
-        print('Error! Please pick a valid option number.')
+    while not user_decision.isdigit() or int(user_decision) > 4 or int(user_decision) < 1:
+        print('Error! "' + user_decision + '" is not a valid option.\nPlease pick a valid number from the list.')
         user_decision = input('''
 What would you like to do? \n
 1) View current balance
@@ -29,12 +29,18 @@ What would you like to do? \n
 
 
 
+
 def program():
     user_decision = get_user_input()
     if int(user_decision) == 1:
-        print(balance)
-    else:
-        print('End program')
+        print('Your balance is $' + balance)
+        new_choice = input('Do you need to do any transactions? ')
+        if new_choice == 'Yes':
+            program()
+        else:
+            print('Thank you, come again!')
+    if int(user_decision) == 4:
+        print('Thank you, come again!')
 
 
 
