3c41f15153367e925cf5fc729b13d4493b4e2e19
Jesse Ruiz <jessejinnaruiz@gmail.com>
2019-03-08 12:07:36 -0600

    Building credit function

--- checkbook.py
+++ checkbook.py
@@ -18,4 +18,36 @@ while not user_decision.isdigit() or int(user_decision) > 5 or input(user_decisi
 3) Record a credit (deposit)
 4) Exit\n''')
     continue
-print(user_decision)
+# print(user_decision)
+
+
+# create a function to format all numerical inputs
+
+
+
+
+
+# create function to run credit (addition/deposit)
+def calculate_transaction(amount):
+    if user_decision == 3:
+        with open('current_balance.txt', 'a') as f:
+            lines = f.readlines()
+            for element in lines:
+
+
+
+# create function to run debit (subtraction/withdrawl)
+
+
+
+
+
+
+
+
+
+
+# --------~~~~~~~~~~ADDITIONAL FEATURES~~~~~~~~~~--------
+
+transactions = []
+historical_dates_times = []
